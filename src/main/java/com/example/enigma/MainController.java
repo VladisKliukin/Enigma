e1ecb48e94cc46c0b69d54b672cfa2e9d13986ff
Vladis <vladiskliukin@gmail.com>
2025-04-12 23:20:08 +0300

    Initial commit

---
+++ src/main/java/com/example/enigma/MainController.java
@@ -0,0 +1,14 @@
+package com.example.enigma;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
