a7b87ab79b4d6ed42e84493d9cbcf83582bc9c3b
Vladis <Vladiskliukin@gmail.com>
2025-04-13 23:21:15 +0300

    Create gui Encrypt

--- src/main/java/com/example/enigma/Controllers/EncryptController.java
+++ src/main/java/com/example/enigma/Controllers/EncryptController.java
@@ -7,11 +7,13 @@ import com.example.enigma.Enums.EWindowType;
 import com.example.enigma.Managers.ManagerWindow;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 
 public class EncryptController extends BaseController {
-    public EncryptController() {
-        ownerWindowType = EWindowType.WINDOW_DECRYPT;
-    }
+
     @FXML
     private ResourceBundle resources;
 
@@ -19,7 +21,45 @@ public class EncryptController extends BaseController {
     private URL location;
 
     @FXML
+    private TextArea ciphertextBox;
+
+    @FXML
     private Button comebackActionButton;
+
+    @FXML
+    private Button createFileNameActionButton;
+
+    @FXML
+    private HBox createFileNameBox;
+
+    @FXML
+    private Button encryptTextActionButton;
+
+    @FXML
+    private TextArea encryptTextBox;
+
+    @FXML
+    private Label encryptionErrorText;
+
+    @FXML
+    private Label erorSupportLanguage;
+
+    @FXML
+    private Label errorSavingToFileText;
+
+    @FXML
+    private HBox keyEncryptBox;
+
+    @FXML
+    private ProgressBar progressBarEvent;
+
+    @FXML
+    private Button saveToFileActionButton;
+
+
+    public EncryptController() {
+        ownerWindowType = EWindowType.WINDOW_DECRYPT;
+    }
 
     @FXML
     void initialize() {
@@ -27,6 +67,13 @@ public class EncryptController extends BaseController {
             ManagerWindow.getInstance().switchTo(this, EWindowType.WINDOW_MAIN);
         });
 
+
     }
 
 }
+
+
+
+
+
+
