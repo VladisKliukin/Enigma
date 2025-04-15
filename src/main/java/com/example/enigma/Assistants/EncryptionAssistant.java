af2ff2ccec2621d1c556aea9a53e6757c4a773cc
StepGameDev <vladisklukin@gmail.com>
2025-04-16 01:15:22 +0300

    create a strong key

--- src/main/java/com/example/enigma/Assistants/EncryptionAssistant.java
+++ src/main/java/com/example/enigma/Assistants/EncryptionAssistant.java
@@ -4,6 +4,7 @@ package com.example.enigma.Assistants;
 import java.util.Random;
 
 public class EncryptionAssistant {
+    final int KEY_LENGTH = 15;
     //---------------------------------------------------------------------------------------------------------
 
     public int isShiftValid(String text) {
@@ -49,10 +50,11 @@ public class EncryptionAssistant {
     //---------------------------------------------------------------------------------------------------------
     public String createEncryptionKey() {
 
-        return  generateStrongKey(15);
+        return generateStrongKey(15);
 
 
     }
+
     //---------------------------------------------------------------------------------------------------------
     private String generateStrongKey(int length) {
         String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -67,6 +69,52 @@ public class EncryptionAssistant {
         for (int i = 0; i < length; i++) {
             key.append(allChars.charAt(random.nextInt(allChars.length())));
         }
+
+        return key.toString();
+    }
+
+
+    /*
+     *вызывавется метод создание ключа
+     * генерируется ключ:
+     * шифрованный текст: wer ghyujki bfsdc vmtgr
+     * key:gubdmr
+     * далее идет проверка
+     * поиск буквы(g) return index = 3;
+     * поиск буквы(u) return index = 6; (6-3) = 3
+     * и тд...
+     * если в key error поиск буквы(j) return index = 7; (7 - 3) = 4 error invalid key
+     * */
+    public String createEncryptionKeyNew(String ciphertext, int shift) {
+
+//Возможно стоит просто доходить до конца строки, и если к примеру у нас в строке всего 7 букв
+// и здвиг к примеру равен 3 тогда получается что у нас будет в ключе только четвертая буква, а 14 букв ключа будут просто рандомные.
+        if (ciphertext.length() <= (KEY_LENGTH * 3)) {
+        }
+
+        StringBuilder key = new StringBuilder();
+
+                int letterIndex = 0;
+
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char currentChar = ciphertext.charAt(i);
+
+
+
+            //чтоб получить второй индекс нам нужно  3+3 = 6
+            // 3
+            // Сравниваем индекс символа с сдвигом
+            if (Character.isLetter(currentChar)) {
+                letterIndex++; // Увеличиваем индекс только для букв
+
+                // Проверяем, совпадает ли текущий индекс с сдвигом
+                if (letterIndex % shift == shift - 1) {
+                    key.append(currentChar);
+                    System.out.println("Символ на индексе " + i + " совпадает с сдвигом: " + currentChar);
+                }
+            }
+        }
+
 
         return key.toString();
     }
