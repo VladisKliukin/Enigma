2cc62289dc7011ec28cf1d77674bd92edf8faff3
Vladis <Vladiskliukin@gmail.com>
2025-04-19 15:11:26 +0300

    create Decrypt

--- src/main/java/com/example/enigma/Controllers/DecryptController.java
+++ src/main/java/com/example/enigma/Controllers/DecryptController.java
@@ -3,10 +3,16 @@ package com.example.enigma.Controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.example.enigma.Assistants.EncryptionAssistant;
 import com.example.enigma.Enums.EWindowType;
 import com.example.enigma.Managers.ManagerWindow;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class DecryptController extends BaseController {
 
@@ -24,9 +30,42 @@ public class DecryptController extends BaseController {
     private Button comebackActionButton;
 
     @FXML
+    private Button createFileNameActionButton;
+
+    @FXML
+    private HBox createFileNameBox;
+
+    @FXML
+    private Button decryptTextActionButton;
+
+    @FXML
+    private TextArea decryptTextBox;
+
+    @FXML
+    private TextArea decryptedTextBox;
+
+    @FXML
+    private Label errorSavingToFileText;
+
+    @FXML
+    private Label errorSupportLanguage;
+
+    @FXML
+    private TextField keyDecryptionText;
+
+    @FXML
+    private HBox keyEncryptBox;
+
+    @FXML
+    private Button openFileActionButton;
+
+    @FXML
+    private ProgressBar progressBarEvent;
+
+    @FXML
     void initialize() {
         comebackActionButton.setOnAction(e -> {
-            ManagerWindow.getInstance().switchTo(this,EWindowType.WINDOW_MAIN);
+            ManagerWindow.getInstance().switchTo(this, EWindowType.WINDOW_MAIN);
         });
 
     }
